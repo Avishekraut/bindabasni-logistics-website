4c46fc6122c90cc6319e159a03d4a94b420d1350
AvishekRaut <avishekraut123@gmail.com>
2025-08-18 13:30:09 +0545

    feat: add services and terminal pages

---
+++ app/services/components/services-card.tsx
@@ -0,0 +1,44 @@
+import type React from "react";
+import Image from "next/image";
+import { Service } from "../page";
+import { AnimatedButton } from "@/components/shared/animated-button";
+
+export function ServicesCard({ servicesData }: { servicesData: Service[] }) {
+  return (
+    <section className="w-full flex flex-col gap-6">
+      {servicesData.map((service) => (
+        <div
+          key={service.id}
+          className="flex flex-col lg:flex-row bg-primary/8 rounded-b-lg md:rounded-lg"
+        >
+          <div className="relative lg:w-1/2 h-64 lg:h-[500px]">
+            <Image
+              src={service.image}
+              alt={service.title}
+              fill
+              className="object-cover rounded-t-lg md:rounded-t-none md:rounded-l-lg"
+              loading="lazy"
+            />
+            <div className="absolute top-6 right-6 w-16 h-16 bg-primary text-white rounded-lg flex items-center justify-center">
+              {service.icon}
+            </div>
+          </div>
+
+          <div className="lg:w-1/2 flex items-center justify-center p-8 lg:p-16">
+            <div className="max-w-lg">
+              <h2 className="text-4xl md:text-5xl font-bold text-slate-800 mb-6 md:mb-8 mt-2 md:mt-4">
+                {service.title}
+              </h2>
+
+              <p className="text-gray-600 text-lg leading-relaxed mb-8">
+                {service.description}
+              </p>
+
+              <AnimatedButton label="Get In Touch" />
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
---
+++ app/terminals/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+import Image from "next/image";
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import PageHeading from "@/components/shared/page-heading";
+import { useRouter } from "next/navigation";
+
+export default function TerminalsPage() {
+  const router = useRouter();
+  const cards = [
+    {
+      image: "/service1.jpg",
+      alt: "ANNAPURNA TERMINALS PVT.LTD",
+      title: <>ANNAPURNA TERMINALS PVT.LTD</>,
+      location: "Bhairahawa",
+      route: "annapurna-terminals-pvt-ltd",
+    },
+    {
+      image: "/service2.jpg",
+      alt: "Logistic services in USA",
+      title: <>TRS HIMALAYAN LOJIPARK PVT.LTD</>,
+      location: "Birgunj",
+      route: "trs-himalayan-lojipark-pvt-ltd",
+    },
+    {
+      image: "/service3.jpg",
+      alt: "Global logistics offers",
+      title: <>PRISTINE VALLEY DRYPORT PVT.LTD</>,
+      location: "Kathmandu",
+      route: "pristine-valley-dryport-pvt-ltd",
+    },
+  ];
+
+  return (
+    <section className="w-full bg-gray-50">
+      <PageHeading
+        title="Terminals"
+        breadcrumb={["Home", "Terminals"]}
+        backgroundImage="/hero-section-bg.png"
+      />
+      <div className="container max-w-7xl mx-auto px-6 sm:px-6 lg:px-2 py-8 md:py-14 md:pb-24">
+        {/* Header Section */}
+        <div className="text-center mb-12">
+          <h2 className="text-4xl md:text-5xl font-bold text-slate-800 mb-2 mt-2 md:mt-4">
+            Our Locations
+          </h2>
+        </div>
+
+        {/* Cards Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {cards.map((card, idx) => (
+            <div
+              key={idx}
+              className={`relative overflow-hidden rounded-3xl h-60 md:h-72 group cursor-pointer}`}
+            >
+              <div className="absolute inset-0">
+                <Image
+                  src={card.image}
+                  alt={card.alt}
+                  fill
+                  className="object-cover"
+                />
+                <div className={`absolute inset-0 bg-black/40`}></div>
+              </div>
+              <div className="relative h-full p-6 md:px-8 md:py-6 flex flex-col justify-end text-white">
+                <div>
+                  <h3 className="text-xl font-bold mb-1 leading-tight">
+                    {card.title}
+                  </h3>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p>{card.location}</p>
+                  <Button
+                    variant="link"
+                    className="text-white hover:text-white p-0 m-0 font-semibold group/btn -ml-3 flex items-center justify-center"
+                    onClick={() => {
+                      router.push(`/terminals/${card.route}`);
+                    }}
+                  >
+                    Details
+                    <ArrowRight className="ml-1 h-4 w-4 transition-transform group-hover/btn:translate-x-1 mt-1" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}

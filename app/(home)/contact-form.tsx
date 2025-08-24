317fb1df73f3270ea03e0ede8138378f04ce7a06
AvishekRaut <avishekraut123@gmail.com>
2025-08-24 12:34:18 +0545

    feat: add contact page

--- app/(home)/contact-form.tsx
+++ app/(home)/contact-form.tsx
@@ -1,7 +1,6 @@
-import { Mail, Phone, Clock } from "lucide-react";
+import { Mail, Phone, Clock, MapPin } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 import { AnimatedButton } from "@/components/shared/animated-button";
 
 export default function ContactForm() {
@@ -19,6 +18,15 @@ export default function ContactForm() {
             elevate your cargo transportation experience.
           </p>
 
+          <div className="flex items-start space-x-4">
+            <div className="flex-shrink-0 w-12 h-12 rounded-full bg-primary border border-white/20 flex items-center justify-center">
+              <Phone className="w-6 h-6 text-white" />
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">Phone</h3>
+              <p className="text-gray-800">014984326</p>
+            </div>
+          </div>
           <div className="space-y-6">
             <div className="flex items-start space-x-4">
               <div className="flex-shrink-0 w-12 h-12 rounded-full bg-primary border  flex items-center justify-center">
@@ -26,29 +34,19 @@ export default function ContactForm() {
               </div>
               <div>
                 <h3 className="text-lg font-semibold">Email</h3>
-                <p className="text-gray-800">contact@shipping.com</p>
+                <p className="text-gray-800">bindabasinilogistic@gmail.com</p>
               </div>
             </div>
 
             <div className="flex items-start space-x-4">
               <div className="flex-shrink-0 w-12 h-12 rounded-full bg-primary border border-white/20 flex items-center justify-center">
-                <Phone className="w-6 h-6 text-white" />
+                <MapPin className="w-6 h-6 text-white" />
               </div>
               <div>
-                <h3 className="text-lg font-semibold">Call Us</h3>
-                <p className="text-gray-800">(00) 112 365 489</p>
-              </div>
-            </div>
-
-            <div className="flex items-start space-x-4">
-              <div className="flex-shrink-0 w-12 h-12 rounded-full bg-primary border border-white/20 flex items-center justify-center">
-                <Clock className="w-6 h-6 text-white" />
-              </div>
-              <div>
-                <h3 className="text-lg font-semibold">
-                  Mon - Sat 9.00 - 18.00
-                </h3>
-                <p className="text-gray-800">Sunday Closed</p>
+                <h3 className="text-lg font-semibold">Address</h3>
+                <p className="text-gray-800">
+                  Nayabazaar Balaju, Valley Cold Store Complex
+                </p>
               </div>
             </div>
           </div>
@@ -83,11 +81,7 @@ export default function ContactForm() {
             rows={8}
             className="bg-gray-50 border border-gray-300 text-gray-800 placeholder:text-gray-500 focus:ring-offset-0 focus:ring-0 resize-none h-40"
           />
-          {/* <Button className="w-full bg-[#FF6B00] hover:bg-[#E05F00] text-white py-3 text-lg font-semibold">
-            Submit Message
-          </Button> */}
           <AnimatedButton label="Submit Message" />
-
         </div>
       </div>
     </div>
---
+++ app/contact/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import PageHeading from "@/components/shared/page-heading";
+import ContactForm from "../(home)/contact-form";
+const Contact = () => {
+  return (
+    <section className="bg-gray-50">
+      <PageHeading
+        title="Contact Us"
+        breadcrumb={["Home", "Contact"]}
+        backgroundImage="/hero-section-bg.png"
+      />
+      <div className="px-6 md:px-16">
+        <ContactForm />
+        <div className="px-6 md:px-32">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1452.439453146231!2d85.30354504847384!3d27.72464122463928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1945b7f099c9%3A0x3ed9247c5c15a94f!2sBindabasini%20Logistics%20Pvt.Ltd.!5e0!3m2!1sen!2snp!4v1755590786914!5m2!1sen!2snp"
+            loading="lazy"
+            className="w-full h-[560px] rounded-lg"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;

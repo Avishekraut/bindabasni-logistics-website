6990ab1500258c97fafd6a6aca583bd1bbd75915
AvishekRaut <avishekraut123@gmail.com>
2025-11-07 12:33:35 +0545

    chore: hide navbar and other components in dashboard route

--- components/BaseLayout/index.tsx
+++ components/BaseLayout/index.tsx
@@ -1,24 +1,36 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Providers } from "@/providers";
 import { Toaster } from "sonner";
 import Navbar from "../shared/navbar";
 import WhatsAppButton from "../shared/whatsapp-button";
 import { Footer } from "../shared/footer";
+import { usePathname } from "next/navigation";
 
 type BaseLayoutProps = {
   children: ReactNode;
 };
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
+  const pathname = usePathname();
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [showFooter, setShowFooter] = useState(true);
+  const [showWhatsApp, setShowWhatsApp] = useState(true);
+
+  useEffect(() => {
+    const isDashboardRoute = pathname.startsWith("/dashboard");
+    setShowNavbar(!isDashboardRoute);
+    setShowFooter(!isDashboardRoute);
+    setShowWhatsApp(!isDashboardRoute);
+  }, [pathname]);
   return (
     <Providers>
       <Toaster richColors />
-      <Navbar />
-      <WhatsAppButton />
+      {showNavbar && <Navbar />}
+      {showWhatsApp && <WhatsAppButton />}
       {children}
-      <Footer />
+      {showFooter && <Footer />}
     </Providers>
   );
 }

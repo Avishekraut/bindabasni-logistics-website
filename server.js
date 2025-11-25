ecc71d9c093c97553fbd817d50cd7ab763e70b74
AvishekRaut <avishekraut123@gmail.com>
2025-11-25 13:15:43 +0545

    chore: server.js update

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const { createServer } = require("http");
-const next = require("next");
+const next = require('next');
+const http = require('http');
 
-const app = next({ dev: false });
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = false;                 
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  http.createServer((req, res) => {
     handle(req, res);
-  }).listen(process.env.PORT || 3000, () => {
-    console.log("Server running...");
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
   });
 });
